390321e3bba261bb8f752d71b601827a1e16bc20
Xavier Arques <xavarques@gmail.com>
2019-04-14 15:22:19 +0200

    Change message

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -4,65 +4,74 @@
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-      const speechText = 'Welcome, you can say Hello or Help. Which would you like to try?';
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .getResponse();
-    }
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText =
+      'Welcome, you can say Hello or Help. Which would you like to try?';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  }
 };
 const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'How are you?';
-      return handlerInput.responseBuilder
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent'
+    );
+  },
+  handle(handlerInput) {
+    const speechText = 'How are you?';
+    return (
+      handlerInput.responseBuilder
         .speak(speechText)
         //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse();
-    }
+        .getResponse()
+    );
+  }
 };
 const HelpIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'You can say hello to me! How can I help?';
-     
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .getResponse();
-    }
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent'
+    );
+  },
+  handle(handlerInput) {
+    const speechText = 'You can say hello to me! How can I help?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  }
 };
 const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-          || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-   handle(handlerInput) {
-      const speechText = 'Goodbye!';
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .getResponse();
-    }
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      (handlerInput.requestEnvelope.request.intent.name ===
+        'AMAZON.CancelIntent' ||
+        handlerInput.requestEnvelope.request.intent.name ===
+          'AMAZON.StopIntent')
+    );
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+    return handlerInput.responseBuilder.speak(speechText).getResponse();
+  }
 };
 const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-      // Any cleanup logic goes here.
-      return handlerInput.responseBuilder.getResponse();
-    }
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
 };
 
 // The intent reflector is used for interaction model testing and debugging.
@@ -70,36 +79,38 @@ const SessionEndedRequestHandler = {
 // for your intents by defining them above, then also adding them to the request
 // handler chain below.
 const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest';
-    },
-    handle(handlerInput) {
-      const intentName = handlerInput.requestEnvelope.request.intent.name;
-      const speechText = `You just triggered ${intentName}`;
- 
-      return handlerInput.responseBuilder
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+  },
+  handle(handlerInput) {
+    const intentName = handlerInput.requestEnvelope.request.intent.name;
+    const speechText = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
         .speak(speechText)
         //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse();
-   }
+        .getResponse()
+    );
+  }
 };
 
 // Generic error handling to capture any syntax or routing errors. If you receive an error
 // stating the request handler chain is not found, you have not implemented a handler for
 // the intent being invoked or included it in the skill builder below.
 const ErrorHandler = {
-    canHandle() {
-      return true;
-    },
-    handle(handlerInput, error) {
-      console.log(`~~~~ Error handled: ${error.message}`);
-      const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.message}`);
+    const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
 
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  }
 };
 
 // This handler acts as the entry point for your skill, routing all request and response
@@ -112,7 +123,7 @@ exports.handler = Alexa.SkillBuilders.custom()
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
-    IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-  .addErrorHandlers(
-    ErrorHandler)
+    IntentReflectorHandler
+  ) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  .addErrorHandlers(ErrorHandler)
   .lambda();
